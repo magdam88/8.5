164910e9ccda3de24e7c72f47ae6c542eacec84e
Magda <Magda@Maczek.local>
2017-02-01 21:59:31 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var a = 3;
+var b = 4;
+var value = (a * a) + (2 * a * b) - (b * b);
+console.log(value);
+
+if (value > 0) {
+    console.log('wynik dodatni');
+} else {
+    console.log('wynik ujemny');
+}
