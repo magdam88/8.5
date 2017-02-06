0f531f05f13aef4da490c4b1966d2d923f2459ef
Magda <Magda@Maczek.local>
2017-02-06 23:14:45 +0100

    added value=0

--- scripts.js
+++ scripts.js
@@ -5,6 +5,27 @@ console.log(value);
 
 if (value > 0) {
     console.log('wynik dodatni');
+}
+if
+else(value = 0) {
+    console.log('wynik rowny 0')
 } else {
     console.log('wynik ujemny');
 }
+
+
+
+/// 8.6
+
+
+function getTriangleArea(a, h) {
+    if ((a <= 0) || (h <= 0)) {
+        console.log('nieprawidłowe dane');
+    } else {
+        return a * h / 2;
+    }
+}
+
+console.log(getTriangleArea(10, 6))
+console.log(getTriangleArea(10, 15))
+console.log(getTriangleArea(0, 5))
